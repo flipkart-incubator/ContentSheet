5e483c50f6668a18b9282a96875183f52fa4f41e
Naveen Chaudhary <naveen.c@flipkart.com>
2018-07-30 20:01:36 +0530

    blur header behaviour

---
+++ ContentSheet/Classes/ContentHeader.swift
@@ -0,0 +1,56 @@
+//
+//  ContentHeader.swift
+//  ContentSheet
+//
+//  Created by Naveen Chaudhary on 30/07/18.
+//
+
+public class ContentHeaderView: UIView {
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.isTranslucent = true
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.isTranslucent = true
+    }
+    
+    public var isTranslucent: Bool = true {
+        didSet {
+            if isTranslucent {
+                self._setBlurrEffect(true)
+            } else {
+                self._setBlurrEffect(false)
+            }
+        }
+    }
+    
+    fileprivate var _blurEffectView: UIVisualEffectView?
+    
+    private func _setBlurrEffect(_ add: Bool) {
+        let onView = self
+        if add {
+            let blurEffectStyle: UIBlurEffectStyle
+            if #available(iOS 10.0, *) {
+                blurEffectStyle = .regular
+            } else {
+                blurEffectStyle = .light
+            }
+            let blurEffect = UIBlurEffect(style: blurEffectStyle)
+            _blurEffectView = UIVisualEffectView(effect: blurEffect)
+            if let blurEffectView = _blurEffectView {
+                blurEffectView.backgroundColor = UIColor.clear
+                blurEffectView.frame = onView.bounds
+                onView.insertSubview(blurEffectView, at: 0)
+            }
+        } else {
+            onView.alpha = 1
+            if let blurEffectView = _blurEffectView {
+                blurEffectView.removeFromSuperview()
+            }
+            _blurEffectView = nil
+        }
+    }
+}
