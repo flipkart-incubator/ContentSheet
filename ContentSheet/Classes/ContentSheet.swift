f3f241e1d17352d0a5cce6d2ae5f6bcfb8fe5a50
Keerthana0504 <38393326+Keerthana0504@users.noreply.github.com>
2019-07-26 02:40:10 +0530

    providing support to open contentSheet as popUp (#33)
    * content sheet height fixed
    
    * hide contentsheet instead of dismiss
    
    * providing support to open contentSheet as popUp
    
    * removing gesture for popup

--- ContentSheet/Classes/ContentSheet.swift
+++ ContentSheet/Classes/ContentSheet.swift
@@ -103,6 +103,7 @@ fileprivate enum PanDirection {
     //Content controller object
     //Not necessarilly a view controller
     fileprivate var _content: ContentSheetContentProtocol
+    fileprivate var _asPopUp: Bool = false
     @objc public var content: ContentSheetContentProtocol {
         get {
             return _content
@@ -282,6 +283,13 @@ fileprivate enum PanDirection {
         self.modalPresentationStyle = .custom
     }
     
+    @objc public required init(content: ContentSheetContentProtocol, asPopUp: Bool) {
+        _content = content
+        _asPopUp = asPopUp
+        super.init(nibName: nil, bundle: nil)
+        self.modalPresentationStyle = .custom
+    }
+    
     //MARK: View lifecycle
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -334,6 +342,9 @@ fileprivate enum PanDirection {
                 var frame = CGRect(x: 0, y: self.view.bounds.height, width: self.view.bounds.width, height: collapsedHeight)
                 _contentContainer.frame = frame
                 
+                if _asPopUp {
+                    frame = CGRect(x: 10, y: (self.view.frame.height - self.collapsedHeight)/2, width: self._contentContainer.frame.width - 20, height: self.collapsedHeight)
+                }
                 if let header = self.contentHeader {
                     _contentContainer.addSubview(header)
                 }
@@ -354,7 +365,11 @@ fileprivate enum PanDirection {
                 
                 self.transitionCoordinator?.animate(alongsideTransition: { (_) in
                     //Animate content
-                    frame.origin.y = self.view.bounds.height - self.collapsedHeight
+                    if self._asPopUp {
+                        frame.origin.y = (self.view.bounds.height - self.collapsedHeight)/2
+                    } else {
+                        frame.origin.y = self.view.bounds.height - self.collapsedHeight
+                    }
                     frame.size.height = self.collapsedHeight
                     self._contentContainer.frame = frame
                     self._layoutContentSubviews()
@@ -386,7 +401,9 @@ fileprivate enum PanDirection {
                 _content.contentSheetDidAddContent?(self)
                 
                 //Check if there is a scrollview to observer
-                _contentContainer.addGestureRecognizer(_panGesture)
+                if !_asPopUp {
+                    _contentContainer.addGestureRecognizer(_panGesture)
+                }
                 
                 //Notify delegate that sheet did show
                 delegate?.contentSheetDidShow?(self)
@@ -485,7 +502,11 @@ fileprivate enum PanDirection {
         self.collapsedHeight = collapsedHeight
         self.expandedHeight = expandedHeight
         
-        let frame = CGRect(x: 0, y: self.view.frame.height - self.collapsedHeight, width: self._contentContainer.frame.width, height: self.collapsedHeight)
+        var frame = CGRect(x: 0, y: self.view.frame.height - self.collapsedHeight, width: self._contentContainer.frame.width, height: self.collapsedHeight)
+        
+        if _asPopUp {
+            frame = CGRect(x: 10, y: (self.view.frame.height - self.collapsedHeight)/2, width: self._contentContainer.frame.width, height: self.collapsedHeight)
+        }
         
         UIView.animate(withDuration: 0.2) {
             self._contentContainer.frame = frame
@@ -646,8 +667,29 @@ extension ContentSheet {
     }
     
     public override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
-        _state = .minimised
-        super.dismiss(animated: flag, completion: completion)
+        if let presentingVC = self.presentingViewController,
+            presentingVC.presentedViewController == self {
+            _state = .minimised
+            super.dismiss(animated: flag, completion: completion)
+        } else {
+            var frame = self.view.frame
+            
+            let progress = self.view.bounds.height - frame.origin.y
+            let duration = (1 - Double(progress))*0.33
+            
+            frame.origin.y = self.view.bounds.height
+
+            UIView.animate(withDuration: duration,
+                           animations: {
+                            self.view.frame = frame
+            }, completion: { [weak self] _ in
+                if let weakSelf = self {
+                    weakSelf.resetContentSheetHeight(collapsedHeight: weakSelf.collapsedHeight, expandedHeight: weakSelf.expandedHeight)
+                    weakSelf.delegate?.contentSheetDidHide?(weakSelf)
+                }
+            })
+
+        }
     }
     
     @objc fileprivate func cancelButtonPressed(_ sender: UIBarButtonItem?) {
